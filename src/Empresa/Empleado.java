4a403cd5395ed24e79eb6251eb81f2da066532b5
Daniela <ddanielasmarquez@gmail.com>
2021-12-01 17:23:40 -0400

    Agragar proyecto Empresa

---
+++ src/Empresa/Empleado.java
@@ -0,0 +1,61 @@
+package Empresa;
+
+public class Empleado  extends Persaona{
+	
+	public String cedula;
+	public String nombre;
+	public String apellido;
+	public int edad;
+	public String telefono ;
+	
+	public float sueldo=0;
+	public String cargo ;
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public float getSueldo() {
+		return sueldo;
+	}
+	public void setSueldo(float sueldo) {
+		this.sueldo = sueldo;
+	}
+	public String getCargo() {
+		return cargo;
+	}
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+	
+	public void  Tipocargo() {
+		if (sueldo >50 && sueldo <500) {
+			System.out.println(" Es un empleado comun " +cargo);
+		}
+		}
+}
---
+++ src/Empresa/Persaona.java
@@ -0,0 +1,59 @@
+package Empresa;
+
+public class Persaona {
+	public String cedula;
+	public String nombre;
+	public String apellido;
+	public int edad;
+	public String telefono ;
+	
+	public Persaona() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	
+	
+	
+	
+
+}
